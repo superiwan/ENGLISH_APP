2792b4bcda58d56d527f19d5d476d55537267206
loveoozi <15935897+love_oozi520@user.noreply.gitee.com>
2026-03-19 20:47:35 +0800

    38

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class EnglishWordApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'English Word App',
+      title: 'English Word App 898989',
       theme: ThemeData(
         useMaterial3: true,
         colorSchemeSeed: Colors.indigo,
